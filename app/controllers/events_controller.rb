704ea133aaa0b8a2811b10cec803f8c4c781d232
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-04 13:16:32 +0100

    edit @event in view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -14,7 +14,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.events.build(events_params)
     if @event.save
        redirect_to user_path(current_user), notice: 'Event created successfully'
     else
