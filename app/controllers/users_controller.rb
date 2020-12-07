5b37fc20e27469e28c44967ef57bd0fb59a69acf
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-07 02:04:45 +0100

    redirect from login_path to user_path in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
   include UsersHelper
-  before_action :authorization, only: [:show]
+  before_action :authorization, except: [:show]
 
-  # def index
-  #   @user = User.all
-  # end
+  def index
+    @user = User.all
+  end
 
   def new
     @user = User.new
@@ -13,8 +13,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'user was successfully created'
-      redirect_to login_path
+      redirect_to user_path(@user)
+      # redirect_to login_path
     else
       render 'new'
     end
