437c8778b3c305576bc745db7b848702fe9defef
root <root@DESKTOP-ESFV8Q4.localdomain>
2020-12-08 12:21:42 +0100

    Style and Add validation to the user views and model respectively

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :events, dependent: :destroy
     has_many :attendances, dependent: :destroy
     has_many :attended_events, class_name: 'Event', foreign_key: 'event_id', through: :attendances        
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
